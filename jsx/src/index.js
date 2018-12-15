570f882d1691436929717ae675b88bcd1a180c7b
federico <federicoviscomi@gmail.com>
2018-12-15 13:58:15 +0000

    section 1

---
+++ jsx/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ const App = () => {
+     return <div>Hi there!</div>;
+ };
+
+RectDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
+
