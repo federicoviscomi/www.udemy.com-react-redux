ee9d83345ff013cbc95e62e6e648b6518ef67564
federico <federicoviscomi@gmail.com>
2018-12-16 13:05:15 +0000

    section 2

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
- const App = () => {
-     return <div>Hi there!</div>;
- };
+function getButtonText() {
+    return "asdfbuttonfdsa";
+}
+
+const App = () => {
+    const buttonText = ["i want", "you to", 1234];
+    const asdf = {
+        asdf: '234'
+    };
+    return (
+        <div style={{border: '1px'}}>
+            <label className="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {getButtonText()}
+            </button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText}
+            </button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {asdf.asdf}
+            </button>
+        </div>
+    );
+};
 
 ReactDOM.render(
     <App/>,
